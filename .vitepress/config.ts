65d6c9384b1a89cfa5338c8c47dcb7ca20749922
moto6 <ultraraptor@naver.com>
2026-03-09 09:47:00 +0900

    feat : 깃헙 배포

--- .vitepress/config.ts
+++
@@ -1,9 +0,0 @@
-import {defineConfig} from 'vitepress'
-
-const REPOSITORY_NAME = 'blog';
-
-export default defineConfig({
-    title: "Cheat Sheet",
-    description: "engineer's cheat sheet",
-    base: `/${REPOSITORY_NAME}/`,
-})
